cce63e4490f1f2b9f901f77de47bd8eec5398bfa
surkun1992@gmail.com <surkun1992@gmail.com>
2024-11-18 19:33:10 +0700

    Добавьте файлы проекта.

---
+++ Html/HtmlMain.cs
@@ -0,0 +1,15 @@
+namespace Html;
+
+public class HtmlMain
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
