80da7fe9654ab1c5fe674d08847ce570085b8b76
surkun1992@gmail.com <surkun1992@gmail.com>
2024-11-21 22:23:00 +0700

    Css Table

---
+++ CssTable/CssTableMain.cs
@@ -0,0 +1,15 @@
+namespace CssTable;
+
+public class CssTableMain
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
