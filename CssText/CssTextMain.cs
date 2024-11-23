5536ca90af1c95217c36d0e3811d482ad22d86ee
surkun1992@gmail.com <surkun1992@gmail.com>
2024-11-23 18:11:48 +0700

    Css tesxt

---
+++ CssText/CssTextMain.cs
@@ -0,0 +1,15 @@
+namespace CssText;
+
+public class CssTextMain
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
