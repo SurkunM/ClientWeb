2b79e18e5f3fd4dde9c9bcfc2d0d733bb676b78d
surkun1992@gmail.com <surkun1992@gmail.com>
2024-12-07 16:06:38 +0700

    Работа с объектами 1.0

---
+++ ObjectsWorking/ObjectWorkingMain.cs
@@ -0,0 +1,15 @@
+namespace ObjectsWorking;
+
+public class ObjectWorkingMain
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
---
+++ ObjectsWorking/wwwroot/script.js
@@ -0,0 +1,40 @@
+﻿"Use strict";
+
+(function () {
+    const countries = [
+        {
+            name: 'Russia',
+            cities: [
+                { name: 'Moscow', population: 12636312 },
+                { name: 'Saint-Petersburg', population: 5600044 },
+                { name: 'Novosibirsk', population: 1635338 }
+            ]
+        },
+        {
+            name: 'USA',
+            cities: [
+                { name: 'Washington', population: 671803 },
+                { name: 'Chicago', population: 2747000 }
+            ]
+        },
+        {
+            name: 'Chaina',
+            cities: [
+                { name: 'Beijing', population: 21893095 },
+                { name: 'Shanghai', population: 26320671 },
+                { name: 'Wuhan', population: 13650432 }
+            ]
+        }
+    ];
+
+    countries
+        .filter(c => c.cities.length === Math.max.apply(null, countries.map(c => c.cities.length)))
+        .forEach(c => console.log(c.name));
+
+    let countriesInformation = {};
+
+    countries
+        .forEach(c => countriesInformation[c.name] = c.cities.reduce((s, c) => s + c.population, 0));
+
+    console.log(countriesInformation);
+})();
