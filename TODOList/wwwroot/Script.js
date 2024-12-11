8e8f7d18eb142d5a56691f37adbb83dac3b3dfef
surkun1992@gmail.com <surkun1992@gmail.com>
2024-12-11 20:26:21 +0700

    TODO List 0.01

---
+++ TODOList/wwwroot/Script.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const todoList = document.getElementById("todo-list");
+    const newTodoForm = document.getElementById("new-todo-form");
+    const newTodoTextField = document.getElementById("new-todo-text-field");
+
+    newTodoForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+       newTodoTextField.classList.remove("invalid");
+
+
+        const newTodoText = newTodoTextField.value.trim();
+
+        if (newTodoText.length === 0) {
+            newTodoTextField.classList.add("invalid");
+            return;
+        }
+
+        const newTodoItem = document.createElement("li");
+        newTodoItem.innerHTML = `
+        <div>
+            <span class="todo-text"></span>
+                <button class="edit-button" type="button">Редактировать</button>
+                <button class="delete-button" type="button">Удалить</button>
+        </div>
+        `;
+
+        newTodoItem.querySelector(".todo-text").textContent = newTodoText;
+
+        todoList.appendChild(newTodoItem);
+    })
+})
