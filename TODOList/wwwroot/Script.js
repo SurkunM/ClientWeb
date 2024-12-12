72cb5434762fb20c523d944d753933a629ed88d3
surkun1992@gmail.com <surkun1992@gmail.com>
2024-12-12 20:24:47 +0700

    Todo List 0.02

--- TODOList/wwwroot/Script.js
+++ TODOList/wwwroot/Script.js
@@ -1,15 +1,15 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     const todoList = document.getElementById("todo-list");
-    const newTodoForm = document.getElementById("new-todo-form");
     const newTodoTextField = document.getElementById("new-todo-text-field");
+    const newTodoForm = document.getElementById("new-todo-form");
 
     newTodoForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
-       newTodoTextField.classList.remove("invalid");
+        newTodoTextField.classList.remove("invalid");
 
 
-        const newTodoText = newTodoTextField.value.trim();
+        let newTodoText = newTodoTextField.value.trim();
 
         if (newTodoText.length === 0) {
             newTodoTextField.classList.add("invalid");
@@ -17,16 +17,60 @@
         }
 
         const newTodoItem = document.createElement("li");
-        newTodoItem.innerHTML = `
-        <div>
-            <span class="todo-text"></span>
-                <button class="edit-button" type="button">Редактировать</button>
-                <button class="delete-button" type="button">Удалить</button>
-        </div>
-        `;
 
-        newTodoItem.querySelector(".todo-text").textContent = newTodoText;
+        function setViewMode() {
+
+            newTodoItem.innerHTML = `
+                <div>
+                    <span class="todo-text"></span>
+                    <button class="edit-button" type="button">Редактировать</button>
+                    <button class="delete-button" type="button">Удалить</button>
+                </div>
+            `;
+
+            newTodoItem.querySelector(".todo-text").textContent = newTodoText;
+
+            newTodoItem.querySelector(".delete-button").addEventListener("click", function () {
+                newTodoItem.remove();
+            });
+
+            newTodoItem.querySelector(".edit-button").addEventListener("click", function () {
+                newTodoItem.innerHTML = `
+                    <div>
+                        <input class="edit-todo-text-field" type="text">
+                        <button class="save-button" type="button">Сохранить</button>
+                        <button class="cancel-button" type="button">Отмена</button>
+                    <div class="error-message">Необходимо задать значение</div>
+                    </div>
+                    `;
+
+                const editTodoTextField = newTodoItem.querySelector(".edit-todo-text-field");
+                editTodoTextField.value = newTodoText;
+
+                newTodoItem.querySelector(".cancel-button").addEventListener("click", function () {
+                    setViewMode();
+                });
+
+                newTodoItem.querySelector(".save-button").addEventListener("click", function () {
+                    const editedTodoText = editTodoTextField.value.trim();
+
+                    if (editedTodoText.length === 0) {
+                        editTodoTextField.classList.add("invalid");
+                        //TODO: стили о ошибке и т.д
+
+                        return;
+                    }
+
+                    newTodoText = editedTodoText;
+                    setViewMode();
+                });
+            });
+        }
+
+        setViewMode();
 
         todoList.appendChild(newTodoItem);
-    })
-})
+
+        newTodoTextField.value = "";
+    });
+});
