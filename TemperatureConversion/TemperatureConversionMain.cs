5fd8d65ef75eb3411fca11e5c66f06994f792d33
surkun1992@gmail.com <surkun1992@gmail.com>
2024-12-14 19:56:17 +0700

    Перевод температур 0.01

---
+++ TemperatureConversion/TemperatureConversionMain.cs
@@ -0,0 +1,15 @@
+namespace TemperatureConversion;
+
+public class TemperatureConversionMain
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
---
+++ TemperatureConversion/wwwroot/Script.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const incomingTemperatureField = document.getElementById("incoming-temperature-field");
+    const temperatureConversionForm = document.getElementById("temperature-conversion-form");
+    const temperatureConversionResult = document.getElementById("temperature-conversion-result");
+
+    temperatureConversionForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        incomingTemperatureField.classList.remove("invalid");
+
+        let incomingTemperature = incomingTemperatureField.value.trim();
+
+        if (incomingTemperature.length === 0) {
+            incomingTemperatureField.classList.add("invalid");
+        }
+
+        const result = document.createElement("label");
+        result.textContent = 100;
+        
+
+        temperatureConversionResult.appendChild(result);
+
+        incomingTemperatureField.value = "";
+    });
+});
