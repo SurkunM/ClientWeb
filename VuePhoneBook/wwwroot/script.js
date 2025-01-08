3d699f9c702830d446b4daa40328cf70f8e18cbd
surkun1992@gmail.com <surkun1992@gmail.com>
2025-01-08 20:20:47 +0700

    Vue Phone Book 0.1

---
+++ VuePhoneBook/wwwroot/script.js
@@ -0,0 +1,30 @@
+﻿class PhoneBookService {
+    constructor() {
+        this.baseUrl = "/api/contacts";
+    }
+    // 1:56 (express.js)
+
+
+    getContacts(term) {
+
+    }
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            contacts: [],
+            firstName: "",
+            lastName: "",
+            phone: ""
+        };
+    },
+
+    methods: {
+        createContact() {
+            
+        }
+    }
+});
+
+app.mount("#app");
