52f23c6d230f6cd10781d555089ccc3b31aa2613
surkun1992@gmail.com <surkun1992@gmail.com>
2025-01-20 19:31:00 +0700

    Работа с объектами (изм 4.0)

--- ObjectsWorking/wwwroot/script.js
+++ ObjectsWorking/wwwroot/script.js
@@ -5,54 +5,54 @@
         {
             name: "Russia",
             cities: [
-                { name: "Moscow", population: 12636312 },
-                { name: "Saint-Petersburg", population: 5600044 },
-                { name: "Novosibirsk", population: 1635338 }
+                {name: "Moscow", population: 12636312},
+                {name: "Saint-Petersburg", population: 5600044},
+                {name: "Novosibirsk", population: 1635338}
             ]
         },
         {
             name: "USA",
             cities: [
-                { name: "Washington", population: 671803 },
-                { name: "Chicago", population: 2747000 }
+                {name: "Washington", population: 671803},
+                {name: "Chicago", population: 2747000}
             ]
         },
         {
             name: "China",
             cities: [
-                { name: "Beijing", population: 21893095 },
-                { name: "Shanghai", population: 26320671 },
-                { name: "Wuhan", population: 13650432 }
+                {name: "Beijing", population: 21893095},
+                {name: "Shanghai", population: 26320671},
+                {name: "Wuhan", population: 13650432}
             ]
         }
     ];
 
-    function getMaxCitiesCountries(array) {
-        const maxCitiesCount = Math.max.apply(null, array.map(c => c.cities.length));
+    function getMaxCitiesCountCountries(countries) {
+        const maxCitiesCount = Math.max.apply(null, countries.map(c => c.cities.length));
 
-        return array.filter(country => country.cities.length === maxCitiesCount);
+        return countries.filter(country => country.cities.length === maxCitiesCount);
     }
 
-    const maxCitiesCountries = getMaxCitiesCountries(countries);
+    const maxCitiesCountCountries = getMaxCitiesCountCountries(countries);
 
     console.log("Страны с максимальным количеством городов:");
-    maxCitiesCountries.forEach(c => console.log(c.name));
+    maxCitiesCountCountries.forEach(c => console.log(c.name));
 
-    function getCountriesNameAndPopulationSumDictionary(array) {
-        const dictionary = {};
+    function getCountryNamesAndTotalPopulationDictionary(array) {
+        const result = {};
 
         array.forEach(country => {
-            return dictionary[country.name] = country.cities.reduce((initialPopulation, city) => initialPopulation + city.population, 0)
+            result[country.name] = country.cities.reduce((result, city) => result + city.population, 0);
         });
 
-        return dictionary;
+        return result;
     }
 
-    const countriesNameAndPopulationSumDictionary = getCountriesNameAndPopulationSumDictionary(countries);
+    const countryNamesAndTotalPopulationDictionary = getCountryNamesAndTotalPopulationDictionary(countries);
 
     console.log("Коллекция стран и суммы населения городов:");
 
-    for (const countryKey in countriesNameAndPopulationSumDictionary) {
-        console.log(`${countryKey}: ${countriesNameAndPopulationSumDictionary[countryKey]}`);
+    for (const countryName in countryNamesAndTotalPopulationDictionary) {
+        console.log(`${countryName}: ${countryNamesAndTotalPopulationDictionary[countryName]}`);
     }
 })();
